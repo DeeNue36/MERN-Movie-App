507f06719548a9562172decb0f8d4c9c1aee2c60
Dugiii <dugidemeg@gmail.com>
2026-07-11 23:23:42 -0500

    Add loginUser controller to userRoutes for authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
+
 //controllers
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
+
 //middleware
 
 const router = express.Router()
 
 router.route('/').post(createUser);
+router.post('/auth', loginUser);
 
 export default router;
