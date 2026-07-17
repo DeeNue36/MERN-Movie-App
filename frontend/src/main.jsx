ff189ef17f11dd77c010ebea2a1721c943bd1987
Dugiii <dugidemeg@gmail.com>
2026-07-16 23:36:12 -0500

    Set up routing and Redux provider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+import { Route, RouterProvider, createRouteFromElements } from 'react-router'
+import { createBrowserRouter } from 'react-router-dom'
+
+
+// Auth
+
+
+
+// Restricted User
+
+
+
+// Routes
+const router = createBrowserRouter(
+  createRouteFromElements(
+    <Route path='/' element={<App />}>
+
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
