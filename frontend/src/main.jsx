6d9af2a18a6d253a1eadff03a0eb128f857fd11e
Dugiii <dugidemeg@gmail.com>
2026-07-27 23:23:20 -0500

    Add login and register routes to main router configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
+import { Login } from './pages/Auth/Login.jsx'
+import { Register } from './pages/Auth/Register.jsx'
 import store from './redux/store.js'
 import { Provider } from 'react-redux'
 import { Route, RouterProvider, createRoutesFromElements } from 'react-router'
@@ -22,6 +24,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 )
