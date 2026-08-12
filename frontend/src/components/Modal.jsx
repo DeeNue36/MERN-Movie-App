a70a89a1ba58ef3b99d243ff7190acfc1cf730ab
Dugiii <dugidemeg@gmail.com>
2026-08-11 23:40:28 -0500

    Refactor button in Modal component for improved accessibility and clarity

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -7,10 +7,15 @@ export const Modal = ({isOpen, onClose, children}) => {
                 <div className="fixed inset-0 flex items-center justify-center z-50">
                     <div className="fixed inset-0 bg-black opacity-50">
                         <div className="absolute top-[40%] left-[20%] bg-white p-4 rounded-lg z-10 text-right">
-                            <button type="button" className="text-black font-semibold hover:text-gray-800 focus:outline-none mr-2" onClick={onClose}>
-                                {/* &times; */} X
+                            
+                            <button type="button" 
+                                className="text-black font-semibold hover:text-gray-800 focus:outline-none mr-2" 
+                                onClick={onClose}
+                            >
+                                &times;
                             </button>
                             {children}
+
                         </div>
                     </div>
                 </div>
