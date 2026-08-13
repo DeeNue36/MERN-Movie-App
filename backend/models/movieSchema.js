0ea2dfe4108a44f9b2c127d5c30ab8417515372d
Dugiii <dugidemeg@gmail.com>
2026-08-12 23:54:44 -0500

    Refactor movieSchema for improved readability and consistency

--- backend/models/movieSchema.js
+++ backend/models/movieSchema.js
@@ -2,39 +2,17 @@ import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
 
 const movieSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-
-    image: {
-        type: String,
-        required: true,
-    },
-
-    year: {
-        type: Number,
-        required: true,
-    },
-
-    genre: {
-        type: ObjectId,
-        ref: 'Genre',
-        required: true
-    },
-
-    description: {
-        type: String,
-        required: true,
-    },
-
-    cast: [{
-        type: String
-    }],
-
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: ObjectId, ref: 'Genre', required: true },
+    description: { type: String, required: true },
+    cast: [{ type: String }],
     reviews: [reviewSchema],
-    totalReviews:{type: Number, default: 0}
-}, {timestamps: true}
+    totalReviews:{type: Number, required: true, default: 0},
+    createdAt: { type: Date, default: Date.now },
+}, 
+    {timestamps: true}
 );
 
 export default mongoose.model('Movie', movieSchema);
