663da75d2fd3a385d56bd1d029827a3558dbc21a
Dugiii <dugidemeg@gmail.com>
2026-08-12 23:48:05 -0500

    Refactor GenreForm component for improved code clarity and organization

--- frontend/src/components/GenreForm.jsx
+++ frontend/src/components/GenreForm.jsx
@@ -4,31 +4,35 @@ export const GenreForm = ({value, setValue, handleSubmit, buttonText = 'Submit',
     return (
         <div className='p-3'>
             <form onSubmit={handleSubmit} className='space-y-3'>
+                {/* Genre Input Field */}
                 <input type="text" name="genre-name" id="genre-name" 
                     className='py-3 px-4 border border-teal-500 rounded-lg w-240 text-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500/50' 
                     placeholder='Enter Genre Name' 
                     value={value} 
                     onChange={(e) => setValue(e.target.value)}
                 />
-
+                
                 <div className="flex justify-between">
+                    {/* Submit Button */}
                     <button 
                         type="submit" 
                         className='bg-teal-500 text-white py-2 px-4 rounded-lg hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500/50'
                     >
                         {buttonText}
                     </button>
+                    
+                    {/* Delete Button */}
+                    {handleDelete && (
+                        <button 
+                            type="button"
+                            onClick={handleDelete} 
+                            className='bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500/50'
+                        >
+                            Delete
+                        </button>
+                    )}
                 </div>
 
-                {handleDelete && (
-                    <button 
-                        type="button"
-                        onClick={handleDelete} 
-                        className='bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500/50'
-                    >
-                        Delete
-                    </button>
-                )}
 
             </form>
         </div>
