be49caadbeb17bdee7774b8376b2b91fa83382f6
Dugiii <dugidemeg@gmail.com>
2026-09-22 21:54:54 -0500

    Add UpdateMovie route to admin section for movie management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,7 @@ import { AdminRoute } from './pages/Admin/AdminRoute.jsx'
 import { GenreList } from './pages/Admin/GenreList.jsx'
 import { CreateMovie } from './pages/Admin/CreateMovie.jsx'
 import { AdminMoviesList } from './pages/Admin/AdminMoviesList.jsx'
+import { UpdateMovie } from './pages/Admin/UpdateMovie.jsx'
 
 
 
@@ -44,6 +45,7 @@ const router = createBrowserRouter(
         <Route path='/admin/movies/genre' element={<GenreList />} />
         <Route path='/admin/movies/create' element={<CreateMovie />} />
         <Route path='/admin/movies-list' element={<AdminMoviesList />} />
+        <Route path='/admin/movies/update/:id' element={<UpdateMovie />} />
       </Route>
     </Route>
   )
