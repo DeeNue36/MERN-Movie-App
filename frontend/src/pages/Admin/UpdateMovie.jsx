ac364e31212b1e8ce4f82f0b8924d929247363d1
Dugiii <dugidemeg@gmail.com>
2026-09-23 22:07:38 -0500

    Add imports for movie update functionality in UpdateMovie component

--- frontend/src/pages/Admin/UpdateMovie.jsx
+++ frontend/src/pages/Admin/UpdateMovie.jsx
@@ -1,4 +1,12 @@
-
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import {
+    useGetSpecificMovieQuery,
+    useUpdateMovieMutation,
+    useUploadImageMutation,
+    useDeleteMovieMutation
+} from '../../redux/api/movies';
+import { toast } from 'react-toastify';
 
 export const UpdateMovie = () => {
     return (
