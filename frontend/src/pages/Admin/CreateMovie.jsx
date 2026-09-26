ba12330a82fc62134811dd0582188326f95f4b74
Dugiii <dugidemeg@gmail.com>
2026-09-25 21:45:46 -0500

    Fix import statement formatting in CreateMovie component

--- frontend/src/pages/Admin/CreateMovie.jsx
+++ frontend/src/pages/Admin/CreateMovie.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { useCreateMovieMutation, useUploadImageMutation } from '../../redux/api/movies'
+import { useCreateMovieMutation, useUploadImageMutation } from '../../redux/api/movies';
 import { useFetchGenresQuery } from '../../redux/api/genre'
 import { toast } from 'react-toastify';
 
